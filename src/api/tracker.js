2613bfe823f63e7357ed4f77fcad9ad166870bb2
Miguel Niblock <miguelniblock@posteo.net>
2021-01-10 21:53:25 -0800

    URI glitch

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {AsyncStorage} from 'react-native';
 
 const instance = axios.create({
-    baseURL: 'http://9dbd6760baf2.ngrok.io'
+    baseURL: 'https://rn-tracks-server.glitch.me/'
 })
 
 instance.interceptors.request.use(
