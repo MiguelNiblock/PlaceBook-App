7bcbc525f41d64fb7f8e6e6fa429d3bb1c431097
Miguel Niblock <miguelniblock@posteo.net>
2021-01-17 10:34:01 -0800

    list of lists in drawer

--- src/components/DrawerContentComponent.js
+++ src/components/DrawerContentComponent.js
@@ -1,10 +1,11 @@
 //custom drawer content component. by default only the routes are shown as links
 import React,{useEffect,useContext} from 'react';
-import {ScrollView, Button } from 'react-native';
+import {ScrollView, FlatList, TouchableOpacity } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 import { DrawerItems } from 'react-navigation-drawer';
 import locationApi from '../api/location';
 import {Context as ListContext} from '../context/ListContext';
+import {ListItem} from 'react-native-elements';
 
 const DrawerContentComponent = (props) => {
 
@@ -22,7 +23,17 @@ const DrawerContentComponent = (props) => {
         forceInset={{ top: 'always', horizontal: 'never' }}
       >
         <DrawerItems {...props} />
-        
+        <FlatList
+          data={state}
+          renderItem={({item})=>{
+            return (
+              <TouchableOpacity>
+                <ListItem title={item.name} />
+              </TouchableOpacity>
+            )
+          }}
+          keyExtractor={item=>item._id}
+        />
       </SafeAreaView>
     </ScrollView>
   );
