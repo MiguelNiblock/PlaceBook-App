860eabd0c5b1bd5c5555e34a39b1c1832b9f0353
Miguel Niblock <miguelniblock@posteo.net>
2021-01-20 02:05:52 -0800

    longPress sets a new marker in mapview

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,15 +1,65 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
+import React,{useEffect,useState,useRef} from 'react';
+import MapView,{Marker} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import * as Permissions from 'expo-permissions';
 
-export default function MapScreen({navigation}) {
+const MapScreen = ({navigation})=>{
+
+  const [showMarker, setShowMaker] = useState(false);
+  const [editMap,setEditMap] = useState(false);
+  const [markerCoords,setMarkerCoords] = useState({});
+  const markerRef = useRef();
+
+  useEffect(()=>{
+    const { status } = Permissions.askAsync(Permissions.LOCATION)
+  })
+
+  const handleLongPress = (e)=>{
+    setMarkerCoords(e.nativeEvent.coordinate)
+    setShowMaker(true)
+    markerRef.current.showCallout();
+    console.log(markerCoords)
+  }
+
   return (
     <View style={styles.container}>
       <Button
         onPress={() => navigation.openDrawer()}
         title="Drawer"
       />
-      <MapView style={styles.map} />
+      
+      <MapView style={editMap ? styles.map : {}}         
+        onMapReady={() => {
+          setEditMap(true)
+        }}
+        initialRegion={{
+          "latitude": 37.421997503686995,
+          "latitudeDelta": 0.018190238622558752,
+          "longitude": -122.08399968221784,
+          "longitudeDelta": 0.01765664666889677,
+        }}
+        provider="google"
+        mapType="hybrid"
+        showsUserLocation
+        showsMyLocationButton
+        zoomControlEnabled
+        onRegionChangeComplete={(region)=>console.log(region)}
+        // onUserLocationChange={}
+        onLongPress={handleLongPress}
+        onPress={()=>setShowMaker(false)}
+      >
+        {showMarker? <Marker draggable
+          ref={markerRef}
+          coordinate={{
+            "latitude": markerCoords.latitude,
+            "latitudeDelta": 0.018190238622558752,
+            "longitude": markerCoords.longitude,
+            "longitudeDelta": 0.01765664666889677,
+          }}
+          pinColor="rgba(0,100,255,1)"
+          title="test"
+        /> : null}
+      </MapView>
       
     </View>
   );
@@ -23,7 +73,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
+    marginLeft: 0,
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height - 150,
-  },
+  }
 });
+
+export default MapScreen;
