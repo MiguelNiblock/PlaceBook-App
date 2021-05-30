28f2f9148252b546ed899632eff5445e2eff1a7e
Miguel Niblock <miguelniblock@posteo.net>
2021-05-29 17:50:00 -0700

    embellishments

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,11 +10,11 @@ const authReducer = (state, action) => {
         case 'add_error':
             return {...state, errorMessage: action.payload};
         case 'signin'://covers both signup & signin
-            return {errorMessage: '', token: action.payload}
+            return {errorMessage: '', token: action.payload};
         case 'clear_error_message':
-            return {...state, errorMessage: ''}
+            return {...state, errorMessage: ''};
         case 'signout':
-            return {token: null, errorMessage: ''}
+            return {token: null, errorMessage: ''};
         default:
             return state;
     }
@@ -34,44 +34,43 @@ const signup = (dispatch) => async ({email,password}) => {
         navigate('mainFlow');
     } catch (err) {
         console.log(err.response.data);
-        dispatch({type:'add_error',payload:'Something went wrong with signup.'})
+        dispatch({type:'add_error',payload:'Something went wrong with signup.'});
     }
-};
-    
+};    
 
 const signin = (dispatch) => async({email,password}) => {
     try {
         //http request to signin route
-        const response = await locationApi.post('/signin',{email,password})
+        const response = await locationApi.post('/signin',{email,password});
         //save token in phone storage
-        await AsyncStorage.setItem('token',response.data.token)
+        await AsyncStorage.setItem('token',response.data.token);
         //update state
-        dispatch({type:'signin',payload:response.data.token})
+        dispatch({type:'signin',payload:response.data.token});
         //navigate to main flow
-        navigate('mainFlow')
+        navigate('mainFlow');
     } catch (err) {
-        dispatch({type:'add_error',payload:'Something went wrong with sign in'})
+        dispatch({type:'add_error',payload:'Something went wrong with sign in'});
     }
 };
 
 const clearErrorMessage = (dispatch) => () => {
-    dispatch({type:'clear_error_message'})
+    dispatch({type:'clear_error_message'});
 }
 
 const tryLocalSignin = dispatch => async() => {
     const token = await AsyncStorage.getItem('token');
     if(token) {
-        dispatch({type:'signin',payload:token})
-        navigate('mainFlow')
+        dispatch({type:'signin',payload:token});
+        navigate('mainFlow');
     } else {
-        navigate('Signup')
+        navigate('loginFlow');
     }
 }
 
 const signout = (dispatch) => async () => {
     await AsyncStorage.removeItem('token');
-    dispatch({type:'signout'})
-    navigate('Signin')
+    dispatch({type:'signout'});
+    navigate('Signin');
 }
 
 //EXPORT
