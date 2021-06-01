d25ea3762540d5a404662fa8cd3af0f92d970d5c
Miguel Niblock <miguelniblock@posteo.net>
2021-05-31 23:04:58 -0700

    listEdit now has a color picker. applies to marers of the list.

--- src/screens/ListEditScreen.js
+++ src/screens/ListEditScreen.js
@@ -1,6 +1,7 @@
 import React,{useState,useContext,useEffect} from 'react';
-import {ScrollView} from 'react-native';
-import {ListItem, Input, Text, Button, CheckBox} from 'react-native-elements';
+import {ScrollView, View} from 'react-native';
+import {ListItem, Input, Text, Button, CheckBox, Overlay} from 'react-native-elements';
+import { TriangleColorPicker } from 'react-native-color-picker'
 import {Context as ListContext} from '../context/ListContext';
 
 const ListEditScreen = ({navigation})=>{
@@ -10,6 +11,7 @@ const ListEditScreen = ({navigation})=>{
   const [listIcon,setListIcon] = useState('');
   const [shown,setShown] = useState(true);
   const {createList,editList,deleteList,state:lists} = useContext(ListContext);
+  const [visible, setVisible] = useState(false);
 
   const listId = navigation.getParam('listId');
 
@@ -34,10 +36,20 @@ const ListEditScreen = ({navigation})=>{
     navigation.goBack();
   };
 
+  const toggleOverlay = ()=>setVisible(!visible)
   return (
     <ScrollView>
       <Input label="List Name" value={listName} onChangeText={setListName} />
-      <Input label="Color" value={listColor} onChangeText={setListColor} />
+      {/* <Input label="Color" value={listColor} onChangeText={setListColor} /> */}
+      <View style={{backgroundColor:listColor,height:50,width:50}} />
+      <Button title="Pick color" onPress={toggleOverlay} />
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} style={{}} >
+        <TriangleColorPicker
+          onColorSelected={color=>{setListColor(color);toggleOverlay()}}
+          style={{height: 400, width: 250}}
+        />
+        <Text>{"\n"}Press the rectangle when done.</Text>
+      </Overlay>
       <Input label="Icon" value={listIcon} onChangeText={setListIcon} />
       <CheckBox title='Shown' checked={shown} onPress={()=>setShown(!shown)} />
       <Button title="Save" onPress={()=>saveList(listId,listName,listColor,listIcon,shown)} />
