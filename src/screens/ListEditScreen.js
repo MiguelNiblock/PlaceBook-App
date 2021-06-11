cd881e6145fbd96654f574cca32038e8b6c3344f
Miguel Niblock <miguelniblock@posteo.net>
2021-06-11 00:31:38 -0700

    List icon picker works New lists have a default icon and color

--- src/screens/ListEditScreen.js
+++ src/screens/ListEditScreen.js
@@ -1,17 +1,20 @@
 import React,{useState,useContext,useEffect} from 'react';
-import {ScrollView, View} from 'react-native';
+import {ScrollView, View, TouchableOpacity} from 'react-native';
 import {ListItem, Input, Text, Button, CheckBox, Overlay} from 'react-native-elements';
 import { TriangleColorPicker } from 'react-native-color-picker'
 import {Context as ListContext} from '../context/ListContext';
+import iconNames from '../hooks/iconNames';
+import {Icon} from 'react-native-elements';
 
 const ListEditScreen = ({navigation})=>{
 
   const [listName,setListName] = useState('');
-  const [listColor,setListColor] = useState('');
-  const [listIcon,setListIcon] = useState('');
+  const [listColor,setListColor] = useState('rgba(0,0,0,1)');
+  const [listIcon,setListIcon] = useState('map-marker');
   const [shown,setShown] = useState(true);
   const {createList,editList,deleteList,state:lists} = useContext(ListContext);
-  const [visible, setVisible] = useState(false);
+  const [showColorPicker, setShowColorPicker] = useState(false);
+  const [showIconPicker, setShowIconPicker] = useState(false);
 
   const listId = navigation.getParam('listId');
 
@@ -36,21 +39,48 @@ const ListEditScreen = ({navigation})=>{
     navigation.goBack();
   };
 
-  const toggleOverlay = ()=>setVisible(!visible)
+  const toggleColorPicker = ()=>setShowColorPicker(!showColorPicker);
+  const toggleIconPicker = ()=>setShowIconPicker(!showIconPicker);
+
   return (
     <ScrollView>
       <Input label="List Name" value={listName} onChangeText={setListName} />
       {/* <Input label="Color" value={listColor} onChangeText={setListColor} /> */}
       <View style={{backgroundColor:listColor,height:50,width:50}} />
-      <Button title="Pick color" onPress={toggleOverlay} />
-      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} style={{}} >
+      <Button title="Choose List Color" onPress={toggleColorPicker} />
+      <Overlay isVisible={showColorPicker} onBackdropPress={toggleColorPicker} style={{}} >
         <TriangleColorPicker
-          onColorSelected={color=>{setListColor(color);toggleOverlay()}}
+          onColorSelected={color=>{setListColor(color);toggleColorPicker()}}
           style={{height: 400, width: 250}}
         />
         <Text>{"\n"}Press the rectangle when done.</Text>
       </Overlay>
-      <Input label="Icon" value={listIcon} onChangeText={setListIcon} />
+      
+      <Icon
+        name={listIcon}
+        type='material-community'
+        color={listColor}
+        size={45}
+      />
+      {/* <Input label="Icon" value={listIcon} onChangeText={setListIcon} /> */}
+      <Button title="Choose List Icon" onPress={toggleIconPicker} />
+      <Overlay isVisible={showIconPicker} onBackdropPress={toggleIconPicker} style={{}} >
+        <ScrollView style={{width: 100}}>
+          {iconNames.map((iconName)=>{
+            return (
+              <TouchableOpacity onPress={()=>{setListIcon(iconName);toggleIconPicker()}} >
+                <Icon
+                  name={iconName}
+                  type='material-community'
+                  color={listColor}
+                  size={45}
+                />
+              </TouchableOpacity>
+            )
+          })}
+        </ScrollView>
+      </Overlay>
+
       <CheckBox title='Shown' checked={shown} onPress={()=>setShown(!shown)} />
       <Button title="Save" onPress={()=>saveList(listId,listName,listColor,listIcon,shown)} />
       {listId && <Button onPress={()=>handleDeleteList(listId)} title='Delete' />} 
