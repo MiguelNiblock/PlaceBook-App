fe5825b7f1265186ed67e7ab3723ba0935f6982c
Miguel Niblock <miguelniblock@posteo.net>
2021-07-08 21:57:09 -0700

    updated actions to list standards

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -13,8 +13,8 @@ const LocationReducer = (state,action) => {
       setLocalData('locs',[...state, action.payload]);
       return [...state, action.payload];
     case 'edit_loc': {
-      console.log('payload:',action.payload);
-      console.log('state:',state);
+      // console.log('payload:',action.payload);
+      // console.log('state:',state);
       const newState = [...state.map(
         (item)=>{
           if(item._id === action.payload._id) {
@@ -42,7 +42,7 @@ const loadLocalLocs = dispatch => async() => {
     let locs = await SecureStore.getItemAsync('locs')
     if (locs) {
       locs = JSON.parse(locs);
-      // console.log('locs from local storage:',locs)
+      console.log('locs from local storage:',locs)
       dispatch({type:'set_locs',payload:locs});
     }
   }
@@ -62,47 +62,53 @@ const fetchLocs = dispatch => async() => {
 };
 
 const createLocation = dispatch => async(name,address,coords,notes,stars,tags,listId) => {
-  console.log('createLoc called')
-  if (await SecureStore.getItemAsync('token')){
+  const _id = uuid.v4();
+  const timeStamp = new Date().toISOString();
+  const newLoc = {_id,name,address,coords,notes,stars,tags,listId,datetimeCreated:timeStamp,datetimeModified:timeStamp}
+  console.log('new loc:',newLoc);
+  try {
+    console.log('trying to POST loc');
     const {data} = await locationApi.post('/locs',{name,address,coords,notes,stars,tags,listId});
+    console.log('createLoc response:',data);
     dispatch({type:'create_loc', payload:data});
-    // console.log('createLocation ran. response:',data);
-    return data;
-  } else {
-    console.log('creating loc locally')
-    const timeStamp = new Date().toISOString();
-    const newLoc = {_id:uuid.v4(),name,address,coords,notes,stars,tags,listId,datetimeCreated:timeStamp,datetimeModified:timeStamp}
-    // console.log('loc created locally:',newLoc)
-    dispatch({type:'create_loc',payload:newLoc});
-    return newLoc;
+    return newLoc
+  } catch(error){
+    console.error(error);
+    console.log('creating loc locally');
+    // queueCreate(newLoc);
+    dispatch({type:'create_loc', payload:newLoc});
+    return newLoc
   }
 };
 
 const editLocation = dispatch => async(locId,name,address,coords,notes,stars,tags,listId) => {
-  console.log('editLoc called');
   const datetimeModified = new Date().toISOString();
-  if (await SecureStore.getItemAsync('token')){
+  try {
+    console.log('trying to PUT loc:',locId,name,address,coords,notes,stars,tags,listId);
     const {data} = await locationApi.put(`/locs/${locId}`,{name,address,coords,notes,stars,tags,listId,datetimeModified});
+    console.log('editLocation response:',data);
     dispatch({type:'edit_loc', payload:data});
-    // console.log('editLocation ran. response:',data);
-    return data;
-  } else {
-    console.log('updating loc locally');
+    return data
+  } catch (error) {
+    console.error(error);
     const updatedLoc = {_id:locId,name,address,coords,notes,stars,tags,listId,datetimeModified};
-    dispatch({type:'edit_loc',payload:updatedLoc});
+    console.log('updating loc locally:',updatedLoc);
+    //queueEdit(updatedLoc);
+    dispatch({type:'edit_loc',payload:updatedLoc});    
     return updatedLoc;
   }
 };
 
 const deleteLocation = dispatch => async(locId) => {
-  console.log('deleteLoc called');
-  if (await SecureStore.getItemAsync('token')){
-    console.log('Trying to DELETE loc in backend')
+  try {
+    console.log('trying to DELETE loc:',locId);
     const {data} = await locationApi.delete(`/locs/${locId}`);
+    console.log('deleteLocation response:',data);
     dispatch({type:'delete_loc',payload:locId});
-    // console.log('deleteLocation ran. response:',data);
-  } else {
+  } catch (error) {
+    console.error(error)
     console.log('deleting loc locally')
+    // queueDelete(locId);
     dispatch({type:'delete_loc',payload:locId});
   }
 };
