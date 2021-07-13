29cbbfb5763496b51d51e5c836d9e547ab95b98b
Miguel Niblock <miguelniblock@posteo.net>
2021-07-13 14:27:49 -0700

    commented unecessary imports

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs'; 
 import { createDrawerNavigator } from 'react-navigation-drawer';
 
 import DrawerContentComponent from './src/components/DrawerContentComponent'
 
 import SigninScreen from './src/screens/SigninScreen';
-import SignupScreen from './src/screens/SignupScreen';
+// import SignupScreen from './src/screens/SignupScreen';
 import AccountScreen from './src/screens/AccountScreen';
 import MapScreen from './src/screens/MapScreen';
 import LocationListScreen from './src/screens/LocationListScreen';
 import LocationEditScreen from './src/screens/LocationEditScreen';
 import ListEditScreen from './src/screens/ListEditScreen';
-import Callouts from './src/screens/Callouts';
+// import Callouts from './src/screens/Callouts';
 import {Provider as AuthProvider} from './src/context/AuthContext';
 import {Provider as ListProvider} from './src/context/ListContext';
 import {Provider as LocationProvider} from './src/context/LocationContext';
@@ -21,11 +20,11 @@ import {Provider as LocationEditProvider} from './src/context/LocationEditContex
 import {Provider as ListQueueProvider} from './src/context/ListQueueContext';
 import {Provider as LocationQueueProvider} from './src/context/LocationQueueContext';
 import {setNavigator}  from './src/navigationRef';
-import ResolveAuthScreen from './src/screens/ResolveAuthScreen'
+// import ResolveAuthScreen from './src/screens/ResolveAuthScreen'
 
-console.disableYellowBox = true;
-// import { LogBox } from "react-native"
-// LogBox.ignoreAllLogs(true)
+// console.disableYellowBox = true;
+import { LogBox } from "react-native"
+LogBox.ignoreAllLogs(true)
 
 const Drawer = createDrawerNavigator({
   Map: MapScreen,
