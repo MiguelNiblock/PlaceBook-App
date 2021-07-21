5fc7bfcf1fda6940e95f1e6c6d6c8d2b860aa78e
Miguel Niblock <miguelniblock@posteo.net>
2021-07-20 21:00:25 -0700

    api timeout

--- src/api/location.js
+++ src/api/location.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
 const instance = axios.create({
-    baseURL: 'https://fierce-depths-47546.herokuapp.com/'
-    // baseURL: 'http://172.16.16.67:3000'
+    baseURL: 'https://fierce-depths-47546.herokuapp.com/',
+    // baseURL: 'http://172.16.16.204:3000',
+    timeout: 5000
 })
 
 instance.interceptors.request.use(
     //fn for success
     async (config) => {
         const token = await SecureStore.getItemAsync('token');
+        // console.log('Token axios:',token);
         if(token){
             config.headers.Authorization = `Bearer ${token}`;
         };
